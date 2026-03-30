dedbb84cb2e9ed0d726279be1c5845b8665d4093
Jillian Golde <jgolde@olin.edu>
2026-03-30 11:35:08 -0400

    Add quintic and trapezoidal generation

--- examples/traj_gen.py
+++ examples/traj_gen.py
@@ -1,12 +1,12 @@
-
 import numpy as np
 
-from funrobo_kinematics.core.trajectory_generator import MultiAxisTrajectoryGenerator, MultiSegmentTrajectoryGenerator
+from funrobo_kinematics.core.trajectory_generator import (
+    MultiAxisTrajectoryGenerator,
+    MultiSegmentTrajectoryGenerator,
+)
 
 
-
-
-class CubicPolynomial():
+class CubicPolynomial:
     """
     Cubic interpolation with position and velocity boundary constraints.
     """
@@ -17,7 +17,6 @@ class CubicPolynomial():
         """
         self.ndof = ndof
 
-    
     def solve(self, q0, qf, qd0, qdf, T):
         """
         Compute cubic polynomial coefficients for each DOF.
@@ -40,22 +39,18 @@ class CubicPolynomial():
         qf = np.asarray(qf, dtype=float)
         qd0 = np.zeros_like(q0) if qd0 is None else np.asarray(qd0, dtype=float)
         qdf = np.zeros_like(q0) if qdf is None else np.asarray(qdf, dtype=float)
-        
-        A = np.array(
-                [[1, t0, t0**2, t0**3],
-                 [0, 1, 2*t0, 3*t0**2],
-                 [1, tf, tf**2, tf**3],
-                 [0, 1, 2*tf, 3*tf**2]
-                ])
 
-        b = np.vstack([
-            q0,
-            qd0,
-            qf,
-            qdf
-        ])
+        A = np.array(
+            [
+                [1, t0, t0**2, t0**3],
+                [0, 1, 2 * t0, 3 * t0**2],
+                [1, tf, tf**2, tf**3],
+                [0, 1, 2 * tf, 3 * tf**2],
+            ]
+        )
+
+        b = np.vstack([q0, qd0, qf, qdf])
         self.coeff = np.linalg.solve(A, b)
-        
 
     def generate(self, t0=0, tf=0, nsteps=100):
         """
@@ -72,35 +67,210 @@ class CubicPolynomial():
         """
         t = np.linspace(t0, tf, nsteps)
         X = np.zeros((self.ndof, 3, len(t)))
-        for i in range(self.ndof): # iterate through all DOFs
+        for i in range(self.ndof):  # iterate through all DOFs
             c = self.coeff[:, i]
 
             q = c[0] + c[1] * t + c[2] * t**2 + c[3] * t**3
             qd = c[1] + 2 * c[2] * t + 3 * c[3] * t**2
             qdd = 2 * c[2] + 6 * c[3] * t
 
-            X[i, 0, :] = q      # position
-            X[i, 1, :] = qd     # velocity
-            X[i, 2, :] = qdd    # acceleration
+            X[i, 0, :] = q  # position
+            X[i, 1, :] = qd  # velocity
+            X[i, 2, :] = qdd  # acceleration
 
         return t, X
 
 
+class QuinticPolynomial:
+    """
+    Quintic interpolation with position and velocity boundary constraints.
+    """
+
+    def __init__(self, ndof=None):
+        """
+        Initialize the trajectory generator.
+        """
+        self.ndof = ndof
+
+    def solve(self, q0, qf, qd0, qdf, T):
+        """
+        Compute quintic polynomial coefficients for each DOF.
+
+        Parameters
+        ----------
+        q0 : array-like, shape (ndof,)
+            Initial positions.
+        qf : array-like, shape (ndof,)
+            Final positions.
+        qd0 : array-like or None, shape (ndof,)
+            Initial velocities. If None, assumed zero.
+        qdf : array-like or None, shape (ndof,)
+            Final velocities. If None, assumed zero.
+        T : float
+            Total trajectory duration.
+        """
+        t0, tf = 0, T
+        q0 = np.asarray(q0, dtype=float)
+        qf = np.asarray(qf, dtype=float)
+        qd0 = np.zeros_like(q0) if qd0 is None else np.asarray(qd0, dtype=float)
+        qdf = np.zeros_like(q0) if qdf is None else np.asarray(qdf, dtype=float)
+        qdd0 = np.zeros_like(q0)
+        qddf = np.zeros_like(q0)
+
+        A = np.array(
+            [
+                [1, t0, t0**2, t0**3, t0**4, t0**5],
+                [0, 1, 2 * t0, 3 * t0**2, 4 * t0**3, 5 * t0**4],
+                [0, 0, 2, 6 * t0, 12 * t0**2, 20 * t0**3],
+                [1, tf, tf**2, tf**3, tf**4, tf**5],
+                [0, 1, 2 * tf, 3 * tf**2, 4 * tf**3, 5 * tf**4],
+                [0, 0, 2, 6 * tf, 12 * tf**2, 20 * tf**3],
+            ]
+        )
+
+        b = np.vstack([q0, qd0, qdd0, qf, qdf, qddf])
+        self.coeff = np.linalg.solve(A, b)
+
+    def generate(self, t0=0, tf=0, nsteps=100):
+        """
+        Generate position, velocity, and acceleration trajectories.
+
+        Parameters
+        ----------
+        t0 : float
+            Start time.
+        tf : float
+            End time.
+        nsteps : int
+            Number of time samples.
+        """
+        t = np.linspace(t0, tf, nsteps)
+        X = np.zeros((self.ndof, 3, len(t)))
+        for i in range(self.ndof):  # iterate through all DOFs
+            c = self.coeff[:, i]
+
+            q = c[0] + c[1] * t + c[2] * t**2 + c[3] * t**3 + c[4] * t**4 + c[5] * t**5
+            qd = (
+                c[1]
+                + 2 * c[2] * t
+                + 3 * c[3] * t**2
+                + 4 * c[4] * t**3
+                + 5 * c[5] * t**4
+            )
+            qdd = 2 * c[2] + 6 * c[3] * t + 12 * c[4] * t**2 + 20 * c[5] * t**3
+
+            X[i, 0, :] = q  # position
+            X[i, 1, :] = qd  # velocity
+            X[i, 2, :] = qdd  # acceleration
+
+        return t, X
+    
+class Trapezoidal:
+    """
+    Trapezoidal interpolation with position and velocity boundary constraints.
+    """
+
+    def __init__(self, ndof=None):
+        """
+        Initialize the trajectory generator.
+        """
+        self.ndof = ndof
+        self.params = {}
+
+    def solve(self, q0, qf, qd0, qdf, T):
+        """
+        Compute trapezoidal parameters for each DOF.
+
+        Parameters
+        ----------
+        q0 : array-like, shape (ndof,)
+            Initial positions.
+        qf : array-like, shape (ndof,)
+            Final positions.
+        qd0 : array-like or None, shape (ndof,)
+            Initial velocities. If None, assumed zero.
+        qdf : array-like or None, shape (ndof,)
+            Final velocities. If None, assumed zero.
+        T : float
+            Total trajectory duration.
+        """
+        t0, tf = 0, T
+        q0 = np.asarray(q0, dtype=float)
+        qf = np.asarray(qf, dtype=float)
+        b1 = (qf - q0) / tf
+        b2 = 2 * (qf - q0) / tf
+        v_min = np.minimum(b1, b2)
+        v_max = np.maximum(b1, b2)
+
+        tb = 0.3 * tf
+        vel = (qf - q0) / (tf - tb)
+        print(vel)
+        vel = np.clip(vel, v_min, v_max)
+        print(vel)
+        alpha = vel/tb
+
+        self.params = {
+            'q0': q0, 'qf': qf, 'vel': vel, 
+            'tb': tb, 'alpha': alpha, 'tf': tf
+        }
+
+    def generate(self, t0=0, tf=0, nsteps=100):
+        """
+        Generate position, velocity, and acceleration trajectories.
+
+        Parameters
+        ----------
+        t0 : float
+            Start time.
+        tf : float
+            End time.
+        nsteps : int
+            Number of time samples.
+        """
+        t = np.linspace(t0, tf, nsteps)
+        X = np.zeros((self.ndof, 3, len(t)))
+        p = self.params
+
+        for i in range(self.ndof):  # iterate through all DOFs
+            q0, qf = p['q0'][i], p['qf'][i]
+            vel, alpha = p['vel'][i], p['alpha'][i]
+            tb, tf_total = p['tb'], p['tf']
+
+            for j, curr_t in enumerate(t):
+                if 0 <= curr_t < tb:
+                    q = q0 + (alpha * curr_t**2)/2
+                    qd = alpha * curr_t
+                    qdd = alpha
+
+                elif tb <= curr_t <= (tf_total - tb):
+                    q = (qf + q0 - vel*tf_total)/2 + vel* curr_t
+                    qd = vel
+                    qdd = 0
+
+                else: 
+                    q = qf - (alpha * tf_total**2)/2 + alpha*tf_total*curr_t - (alpha*curr_t**2)/2
+                    qd = alpha*tf_total - alpha*curr_t
+                    qdd = -alpha
+
+
+                X[i, 0, j] = q  # position
+                X[i, 1, j] = qd  # velocity
+                X[i, 2, j] = qdd  # acceleration
+
+        return t, X
 
 
 def main():
     ndof = 2
-    method = CubicPolynomial(ndof=ndof)
+    method = Trapezoidal(ndof=ndof)
     mode = "joint"
 
     # --------------------------------------------------------
     # Point-to-point multi-axis trajectory generator
     # --------------------------------------------------------
 
-    # traj = MultiAxisTrajectoryGenerator(method=method,
-    #                                     mode=mode,
-    #                                     ndof=ndof)
-    
+    # traj = MultiAxisTrajectoryGenerator(method=method, mode=mode, ndof=ndof)
+
     # traj.solve(q0=-30, qf=60, T=1)
     # traj.generate(nsteps=20)
 
@@ -108,18 +278,19 @@ def main():
     # Via point multi-axis trajectory generator
     # --------------------------------------------------------
 
-    traj = MultiSegmentTrajectoryGenerator(method=method,
-                                           mode=mode,
-                                           ndof=ndof,
-                                            )
+    traj = MultiSegmentTrajectoryGenerator(
+        method=method,
+        mode=mode,
+        ndof=ndof,
+    )
     via_points = [[-30, 30], [0, 45], [30, 15], [50, -30]]
 
     traj.solve(via_points, T=2)
     traj.generate(nsteps_per_segment=20)
-    
-    
+
     # plotter
     traj.plot()
+
 
 if __name__ == "__main__":
     main()
--- funrobo_kinematics/core/trajectory_generator.py
+++ funrobo_kinematics/core/trajectory_generator.py
@@ -2,34 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-class MultiAxisTrajectoryGenerator():
+class MultiAxisTrajectoryGenerator:
     """
     Multi-axis trajectory generator for joint or task space trajectories.
 
     Supports cubic, quintic polynomial, and trapezoidal velocity profiles.
     """
-    
-    def __init__(self, method=None,
-                 mode="joint",
-                 ndof=1,
-                 ):
+
+    def __init__(
+        self,
+        method=None,
+        mode="joint",
+        ndof=1,
+    ):
         """
         Initialize the trajectory generator with the given configuration.
         """
         if method is None:
             raise ValueError("Trajectory generation method must be specified.")
-        
+
         self.ndof = ndof
         self.method = method
-        
+
         if mode == "joint":
             self.mode = "Joint Space"
-            self.labels = [f'axis{i+1}' for i in range(self.ndof)]
+            self.labels = [f"axis{i+1}" for i in range(self.ndof)]
         elif mode == "task":
             self.mode = "Task Space"
-            self.labels = ['x', 'y', 'z']  
-
+            self.labels = ["x", "y", "z"]
 
     def solve(self, q0, qf, qd0=None, qdf=None, qdd0=None, qddf=None, T=1):
         """
@@ -39,7 +39,7 @@ class MultiAxisTrajectoryGenerator():
             q0   : start position
             qf   : final position
             qd0  : start velocity (optional)
-            qdf  : final velocity (optional)
+            qdf  : final velocity (optional) 
             qdd0 : start acceleration (optional)
             qddf : final acceleration (optional)
             T    : duration of the trajectory
@@ -47,7 +47,6 @@ class MultiAxisTrajectoryGenerator():
         self.T = T
         self.method.solve(q0, qf, qd0, qdf, T=T)
 
-    
     def generate(self, nsteps=100):
         """
         Generate the trajectory at discrete time steps.
@@ -59,45 +58,46 @@ class MultiAxisTrajectoryGenerator():
         """
         self.t, self.X = self.method.generate(tf=self.T, nsteps=nsteps)
 
-
     def plot(self):
         """
         Plot the position, velocity, and acceleration trajectories.
         """
         self.fig = plt.figure()
-        self.sub1 = self.fig.add_subplot(3,1,1)  # Position plot
-        self.sub2 = self.fig.add_subplot(3,1,2)  # Velocity plot
-        self.sub3 = self.fig.add_subplot(3,1,3)  # Acceleration plot
+        self.sub1 = self.fig.add_subplot(3, 1, 1)  # Position plot
+        self.sub2 = self.fig.add_subplot(3, 1, 2)  # Velocity plot
+        self.sub3 = self.fig.add_subplot(3, 1, 3)  # Acceleration plot
 
-        self.fig.set_size_inches(8, 10)    
-        self.fig.suptitle(self.mode + " Trajectory Generator (Point-to-Point)", fontsize=16)
+        self.fig.set_size_inches(8, 10)
+        self.fig.suptitle(
+            self.mode + " Trajectory Generator (Point-to-Point)", fontsize=16
+        )
 
-        colors = ['r', 'g', 'b', 'm', 'y']
+        colors = ["r", "g", "b", "m", "y"]
 
         for i in range(self.ndof):
             # position plot
-            self.sub1.plot(self.t, self.X[i][0], colors[i]+'o-', label=self.labels[i])
-            self.sub1.set_ylabel('position', fontsize=15)
+            self.sub1.plot(self.t, self.X[i][0], colors[i] + "o-", label=self.labels[i])
+            self.sub1.set_ylabel("position", fontsize=15)
             self.sub1.grid(True)
             self.sub1.legend()
-        
+
             # velocity plot
-            self.sub2.plot(self.t, self.X[i][1], colors[i]+'o-', label=self.labels[i])
-            self.sub2.set_ylabel('velocity', fontsize=15)
+            self.sub2.plot(self.t, self.X[i][1], colors[i] + "o-", label=self.labels[i])
+            self.sub2.set_ylabel("velocity", fontsize=15)
             self.sub2.grid(True)
             self.sub2.legend()
 
             # acceleration plot
-            self.sub3.plot(self.t, self.X[i][2], colors[i]+'o-', label=self.labels[i])
-            self.sub3.set_ylabel('acceleration', fontsize=15)
-            self.sub3.set_xlabel('Time (secs)', fontsize=18)
+            self.sub3.plot(self.t, self.X[i][2], colors[i] + "o-", label=self.labels[i])
+            self.sub3.set_ylabel("acceleration", fontsize=15)
+            self.sub3.set_xlabel("Time (secs)", fontsize=18)
             self.sub3.grid(True)
             self.sub3.legend()
 
         plt.show()
-        
 
-class MultiSegmentTrajectoryGenerator():
+
+class MultiSegmentTrajectoryGenerator:
     """
     Multi-segment trajectory generator.
 
@@ -105,28 +105,24 @@ class MultiSegmentTrajectoryGenerator():
         t : (N,)
         X : (ndof, 3, N)
     """
-    
-    def __init__(self, method=None,
-                 mode="joint",
-                 ndof=1
-                 ):
+
+    def __init__(self, method=None, mode="joint", ndof=1):
         """
         Initialize the trajectory generator with the given configuration.
         """
         if method is None:
             raise ValueError("Trajectory generation method must be specified.")
-        
+
         self.ndof = ndof
         self.method = method
 
         if mode == "joint":
             self.mode = "Joint Space"
-            self.labels = [f'axis{i+1}' for i in range(self.ndof)]
+            self.labels = [f"axis{i+1}" for i in range(self.ndof)]
         elif mode == "task":
             self.mode = "Task Space"
-            self.labels = ['x', 'y', 'z']
+            self.labels = ["x", "y", "z"]
 
-    
     def solve(self, waypoints, T):
         """
         Fit the trajectories and solve for the trajectory parameters.
@@ -150,23 +146,24 @@ class MultiSegmentTrajectoryGenerator():
             qf = wp[i + 1]
 
             # Velocity continuity at waypoints
-            if i == 0: # first segment, start velocity is zero
+            if i == 0:  # first segment, start velocity is zero
                 qd0 = np.zeros(self.ndof)
             else:
                 qd0 = (wp[i] - wp[i - 1]) / self.T
 
-            if i == self.n_segments - 1: # last segment, final velocity is zero
+            if i == self.n_segments - 1:  # last segment, final velocity is zero
                 qdf = np.zeros(self.ndof)
             else:
                 qdf = (wp[i + 1] - wp[i]) / self.T
 
             print(f"Segment {i+1}: q0={q0}, qf={qf}, qd0={qd0}, qdf={qdf}")
 
-            model = type(self.method)(ndof=self.ndof) # creates a new instance of the trajectory gen class
+            model = type(self.method)(
+                ndof=self.ndof
+            )  # creates a new instance of the trajectory gen class
             model.solve(q0, qf, qd0, qdf, T=T)
             self.segment_models.append(model)
 
-    
     def generate(self, nsteps_per_segment=100):
         """
         Generate trajectory.
@@ -180,13 +177,15 @@ class MultiSegmentTrajectoryGenerator():
         """
         segments_X = []
 
-        total_nsteps = (nsteps_per_segment-1) * self.n_segments + 1 # to avoid duplicate points at segment boundaries
+        total_nsteps = (
+            nsteps_per_segment - 1
+        ) * self.n_segments + 1  # to avoid duplicate points at segment boundaries
         self.t = np.linspace(0, self.T, total_nsteps)
 
         for i, model in enumerate(self.segment_models):
             _, X_ = model.generate(tf=self.T, nsteps=nsteps_per_segment)
 
-            if i > 0: # Avoid duplicate points at boundaries
+            if i > 0:  # Avoid duplicate points at boundaries
                 X_ = X_[:, :, 1:]
 
             segments_X.append(X_)
@@ -194,7 +193,6 @@ class MultiSegmentTrajectoryGenerator():
         # Concatenate all segments
         self.X = np.concatenate(segments_X, axis=2)
 
-    
     def plot(self):
         """
         Plot trajectory (position, velocity, acceleration).
@@ -207,15 +205,15 @@ class MultiSegmentTrajectoryGenerator():
         fig, axs = plt.subplots(3, 1, figsize=(10, 10), sharex=True)
         fig.suptitle("Multi-Segment Trajectory", fontsize=16)
 
-        labels = [f'axis{i+1}' for i in range(self.ndof)]
-        colors = ['r', 'g', 'b', 'm', 'y']
+        labels = [f"axis{i+1}" for i in range(self.ndof)]
+        colors = ["r", "g", "b", "m", "y"]
 
         for i in range(self.ndof):
-            c = colors[i]+'o-'
+            c = colors[i] + "o-"
 
-            axs[0].plot(self.t, self.X[i, 0, :], c, label=labels[i]) # Position
-            axs[1].plot(self.t, self.X[i, 1, :], c, label=labels[i]) # Velocity
-            axs[2].plot(self.t, self.X[i, 2, :], c, label=labels[i]) # Acceleration
+            axs[0].plot(self.t, self.X[i, 0, :], c, label=labels[i])  # Position
+            axs[1].plot(self.t, self.X[i, 1, :], c, label=labels[i])  # Velocity
+            axs[2].plot(self.t, self.X[i, 2, :], c, label=labels[i])  # Acceleration
 
         axs[0].set_ylabel("Position")
         axs[1].set_ylabel("Velocity")
@@ -228,6 +226,3 @@ class MultiSegmentTrajectoryGenerator():
 
         plt.tight_layout()
         plt.show()
-
-
-
